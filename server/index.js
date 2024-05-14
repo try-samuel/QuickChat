e432c86d9ac49da605ecdb11b6e5109a3fccd43b
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-05-14 10:47:06 +0100

    done with basics on server

--- server/index.js
+++ server/index.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-app.post("/signup", async (req, res) => {
-  return {};
-});
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
 
-app.post("/login", async (req, res) => {
-  return {};
+  try {
+    const putRes = await axios.put(
+      "https://api.chatengine.io/users/",
+      {
+        username: username,
+        secret: username,
+        first_name: username,
+      },
+      { headers: { "private-key": process.env.PRIVATE_KEY } }
+    );
+    return res.status(putRes.status).json(putRes.data);
+  } catch (err) {
+    return res.status(err.response.status).json(err.response.data);
+  }
 });
 
 app.listen(3001);
